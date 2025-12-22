0c932cc6c6b5d61306f8df1e5931447ea1fc82f4
adith2005-20 <132918418+adith2005-20@users.noreply.github.com>
2025-12-22 15:16:35 +0530

    init: initialized repo

---
+++ go.mod
@@ -0,0 +1,10 @@
+module github.com/adith2005-20/hush
+
+go 1.24.3
+
+require (
+	github.com/spf13/cobra v1.8.0
+	github.com/mattn/go-sqlite3 v1.14.18
+	gopkg.in/yaml.v3 v3.0.1
+	golang.org/x/crypto/pbkdf2 v0.17.0
+)
---
+++ pkg/crypto/crypto.go
@@ -0,0 +1,77 @@
+package crypto
+
+import (
+	"crypto/aes"
+	"crypto/cipher"
+	"crypto/rand"
+	"crypto/sha256"
+	"encoding/base64"
+	"errors"
+	"io"
+
+	"golang.org/x/crypto/pbkdf2"
+)
+
+func DeriveKey(passphrase string, salt []byte) []byte {
+	return pbkdf2.Key([]byte(passphrase), salt, 100000, 32, sha256.New)
+}
+
+func Encrypt(plaintext string, key []byte) (string, error) {
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		return "", err
+	}
+
+	gcm, err := cipher.NewGCM(block)
+	if err != nil {
+		return "", err
+	}
+
+	nonce := make([]byte, gcm.NonceSize())
+
+	if _, err := io.ReadFull(rand.Reader, nonce); err != nil {
+		return "", err
+	}
+
+	ciphertext := gcm.Seal(nonce, nonce, []byte(plaintext), nil)
+	return base64.StdEncoding.EncodeToString(ciphertext), nil
+}
+
+func Decrypt(ciphertext string, key []byte) (string, error) {
+	data, err := base64.StdEncoding.DecodeString(ciphertext)
+	if err != nil {
+		return "", err
+	}
+
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		return "", err
+	}
+
+	gcm, err := cipher.NewGCM(block)
+	if err != nil {
+		return "", err
+	}
+
+	nonceSize := gcm.NonceSize()
+
+	if len(data) < nonceSize {
+		return "", errors.New("Ciphertext too short.")
+	}
+
+	nonce := data[:nonceSize]
+	ct := data[nonceSize:]
+
+	plaintext, err := gcm.Open(nil, nonce, ct, nil)
+	if err != nil {
+		return "", err
+	}
+
+	return string(plaintext), nil
+}
+
+func GenerateSalt() ([]byte, error) {
+	salt := make([]byte, 32)
+	_, err := rand.Read(salt)
+	return salt, err
+}
